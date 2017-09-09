a51a5548a937844c47a02a9348ff96396e227914
herb123456 <herb123456@gmail.com>
2017-09-09 13:53:41 +0800

    add Read uncommitted level

---
+++ read_uncommitted/create_product.php
@@ -0,0 +1,21 @@
+<?php
+// create_product.php <name>
+require_once "bootstrap.php";
+
+$newProductName = $argv[1];
+
+/**
+ * @var \Doctrine\DBAL\Connection $conn
+ */
+$conn->beginTransaction();
+
+$product = new Product();
+$product->setName($newProductName);
+
+$entityManager->persist($product);
+$entityManager->flush();
+
+sleep(1000000);
+$conn->rollBack();
+
+echo "Created Product with ID " . $product->getId() . "\n";
---
+++ read_uncommitted/list_product.php
@@ -0,0 +1,13 @@
+<?php
+// list_products.php
+require_once "bootstrap.php";
+
+$productRepository = $entityManager->getRepository('Product');
+$products = $productRepository->findAll();
+
+foreach ($products as $product) {
+    /**
+     * @var Product $product
+     */
+    echo sprintf("%s-%s\n", $product->getId(), $product->getName());
+}
